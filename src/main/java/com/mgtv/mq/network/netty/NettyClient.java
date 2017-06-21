56986b5fbd7a5d47c29a56f700bcdc4eaf093af1
刘波涛 <liubotao@liubotaodeMacBook-Pro.local>
2017-06-21 11:21:45 +0800

    time

--- src/main/java/com/mgtv/mq/network/netty/NettyClient.java
+++ src/main/java/com/mgtv/mq/network/netty/NettyClient.java
@@ -2,10 +2,13 @@ package com.mgtv.mq.network.netty;
 
 import com.mgtv.mq.protocol.Command;
 import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.ByteBuf;
 import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
+
+import java.util.Date;
 
 
 public class NettyClient {
@@ -36,12 +39,17 @@ public class NettyClient {
     }
 }
 
-
 class ProtocolClient extends ChannelInboundHandlerAdapter {
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-        System.out.println("channelRead");
-        super.channelRead(ctx, msg);
+        ByteBuf m = (ByteBuf) msg;
+        try {
+            long currentTimeMillis = (m.readUnsignedInt() - 2208988800L) * 1000L;
+            System.out.println(new Date(currentTimeMillis));
+            ctx.close();
+        } finally {
+            m.release();
+        }
     }
 
     @Override
